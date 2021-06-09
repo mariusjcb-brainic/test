9f47633421ec8f15cf1d3e9135c44ad7ad728889
Marius Ilie <marius@iliemarius.ro>
2021-06-09 13:48:29 +0300

    Initial Commit

---
+++ Shared/testXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  testXcodeCloudApp.swift
+//  Shared
+//
+//  Created by Marius Ilie on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct testXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
